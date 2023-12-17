8bb0470d8a6f94589cf0a221e05f89f34f9b10f5
dominhnghia123 <104440701+dominhnghia123@users.noreply.github.com>
2023-12-17 21:16:13 +0700

    Update CartMedia.java

--- AIMS/src/entity/cart/CartMedia.java
+++ AIMS/src/entity/cart/CartMedia.java
@@ -71,6 +71,7 @@ public class CartMedia {
      * @return String
      */
     @Override
+    //Single Responsibility Principle
     public String toString() {
         return "{"
                 + " media='" + media + "'"
